3add21b165f1cbdff44c30f93dff533487c55ea5
Giulio Cervera <giulio.cervera@gmail.com>
2012-01-06 06:59:09 -0500

    E730: button backlight dim
    Change-Id: Ia04eaddff4614d4a0970685be76145072afbc37f

--- liblights/lights.c
+++ liblights/lights.c
@@ -76,22 +76,28 @@ static void set_speaker_light_locked (struct light_device_t *dev, struct light_s
 static void handle_speaker_battery_locked (struct light_device_t *dev) {
 }
 
-static int set_light_buttons (struct light_device_t* dev,
-		struct light_state_t const* state) {
-	int err = 0;
-	int on = is_lit (state);
-	pthread_mutex_lock (&g_lock);
-	err = write_int (BUTTON_FILE, on ? 5 : 0); /* Range: 0 - 20 */
-	pthread_mutex_unlock (&g_lock);
-
-	return 0;
-}
-
 static int rgb_to_brightness(struct light_state_t const* state)
 {
     int color = state->color & 0x00ffffff;
     return ((77*((color>>16)&0x00ff))
             + (150*((color>>8)&0x00ff)) + (29*(color&0x00ff))) >> 8;
+}
+
+static int set_light_buttons (struct light_device_t* dev,
+		struct light_state_t const* state) {
+	int err = 0;
+	int on = is_lit (state);
+	int brightness = rgb_to_brightness(state) * 5 / 255;
+
+	if (brightness < 1)
+		brightness = 1;
+	LOGV("%s brightness=%d", __func__, brightness);
+
+	pthread_mutex_lock (&g_lock);
+	err = write_int (BUTTON_FILE, on ? brightness : 0);
+	pthread_mutex_unlock (&g_lock);
+
+	return 0;
 }
 
 static int set_light_backlight(struct light_device_t* dev,
